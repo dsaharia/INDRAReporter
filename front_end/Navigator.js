3bca6bfbf80b0dca494cbdfcbe9b022222de1f66
Dhiraj Saharia <dsaharia@outlook.com>
2019-05-05 15:58:37 +0530

    navigator added

--- front_end/Navigator.js
+++ front_end/Navigator.js
@@ -1,8 +1,34 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Flood from "./src/screens/Flood";
+import Rain from "./src/screens/Rain";
+import Indra from './src/screens/Indra'
 
 const Navigator = createStackNavigator({
-    Flood: { screen: Flood },
-});
-const NavigatorApp = createAppContainer(Navigator);
-export default NavigatorApp;
+    Flood: {
+        screen: Flood,
+    navigationOptions: {
+        title: 'Flood',
+        },
+    },
+    Rain: {
+        screen: Rain,
+        navigationOptions: {
+          title: 'Rain',
+        },
+    },
+    Indra: {
+        screen: Indra,
+        navigationOptions: {
+        title: 'Indra',
+        header: null,
+        }
+    },
+
+},
+{
+    initialRouteName: 'Indra'
+}
+);
+
+
+export default Navigator;
