e02a6257a8099a1efb6ba131601a130dcd59fcad
Dhiraj Saharia <dsaharia@outlook.com>
2019-05-30 16:11:21 +0530

    location data accessed using geolocationAPI

--- front_end/src/screens/Flood.js
+++ front_end/src/screens/Flood.js
@@ -3,19 +3,46 @@ import {
     View,
     StyleSheet,
     Text,
-    Button,
     Alert,
+    TouchableOpacity,
 } from 'react-native';
 
 export default class Flood extends Component {
+    constructor(){
+        super();
+        this.state = {
+            latitude: null,
+            longitude: null,
+            error: null
+        };
+    }
+    getLocation = () => {
+        let geoOptions = {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 1000,
+        };
+        navigator.geolocation.getCurrentPosition(
+            position => {   
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    error: null,
+                });
+            },
+            error => Alert.alert(error.message), geoOptions);
+        
+    }
     render() {
-        onPressFlood = () => {
-            Alert.alert(" Flood ");
-        }
         return (
             <View style={styles.container}>
-                <Text>FLOOD</Text>
+                <TouchableOpacity onPress = {this.getLocation} style={styles.button}>
+                    <Text style={styles.floodText} >LATITUDE: {this.state.latitude}</Text>
+                    <Text style={styles.floodText} >LONGITUDE: {this.state.longitude}</Text>
+                    {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+                </TouchableOpacity>
             </View>
+            
         );
     }
 }
@@ -31,4 +58,9 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontFamily: 'monospace',
     },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10
+      },
 });
