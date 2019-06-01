b6cd1788eb1aa2628e6075d83aee286a00703654
Dhiraj Saharia <dsaharia@outlook.com>
2019-06-01 18:39:33 +0530

    sqlite storage data

--- front_end/android/app/src/main/java/com/indra/MainApplication.java
+++ front_end/android/app/src/main/java/com/indra/MainApplication.java
@@ -12,6 +12,8 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
+import org.pgsqlite.SQLitePluginPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -23,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
+        new SQLitePluginPackage(),   // register SQLite Plugin he
           new MainReactPackage(),
             new RNGestureHandlerPackage()
       );
