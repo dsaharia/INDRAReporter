d6022c34f494daa2d1fc4d047aa5c1443e7df868
Dhiraj Saharia <dsaharia@outlook.com>
2019-06-01 18:40:34 +0530

    added location data insert and retrieve using sqlite

--- front_end/src/screens/Flood.js
+++ front_end/src/screens/Flood.js
@@ -6,6 +6,13 @@ import {
     Alert,
     TouchableOpacity,
 } from 'react-native';
+// import SQLite from 'react-native-sqlite-storage';
+
+var SQLite = require('react-native-sqlite-storage')
+        var db = SQLite.openDatabase({name: 'a', createFromLocation : "indra_test.db"}, () => {},
+        error => {
+          Alert.alert("ERROR");
+        });
 
 export default class Flood extends Component {
     constructor(){
@@ -15,8 +22,25 @@ export default class Flood extends Component {
             longitude: null,
             error: null
         };
-    }
-    getLocation = () => {
+
+        }
+        sendData = () => {
+            db.transaction(txn => {
+                txn.executeSql(
+                //   "INSERT INTO `Test` (latitude,longitude) VALUES(?,?)",
+                'SELECT * FROM Test',
+                //   [this.state.latitude,this.state.longitude],
+                [],
+                  (txn, res) => {
+                      for(let i=0;i<res.rows.length;i++){
+                        console.log(res.rows.item(i));
+                      }
+                      console.log()
+                  }
+                );
+              }) 
+        }
+      getLocation = () => {
         let geoOptions = {
             enableHighAccuracy: true,
             timeout: 20000,
@@ -29,17 +53,25 @@ export default class Flood extends Component {
                     longitude: position.coords.longitude,
                     error: null,
                 });
+                
+                
             },
             error => Alert.alert(error.message), geoOptions);
         
     }
     render() {
+        
         return (
+            
             <View style={styles.container}>
                 <TouchableOpacity onPress = {this.getLocation} style={styles.button}>
                     <Text style={styles.floodText} >LATITUDE: {this.state.latitude}</Text>
                     <Text style={styles.floodText} >LONGITUDE: {this.state.longitude}</Text>
                     {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+                    
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.sendData} style={styles.button}>
+                    <Text> SEND DATA</Text>
                 </TouchableOpacity>
             </View>
             
