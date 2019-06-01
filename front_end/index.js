bb2aac2d895e3bdb77d81c2875cb215ec1c62470
Dhiraj Saharia <dsaharia@outlook.com>
2019-06-01 18:39:58 +0530

    using sqlite db storing and viewing data

--- front_end/index.js
+++ front_end/index.js
@@ -3,5 +3,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+var SQLite = require('react-native-sqlite-storage')
 AppRegistry.registerComponent(appName, () => App);
