0c542c2493baad3c82af3985a321b57a4230a5e5
Dhiraj Saharia <dsaharia@outlook.com>
2019-06-13 19:32:30 +0530

    converted buttons to touchableopacity

--- front_end/Buttons.js
+++ front_end/Buttons.js
@@ -2,24 +2,51 @@
 import React, {Component} from 'react';
 import {
     View,
-    Button,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
 } from 'react-native';
 
 export default class Buttons extends Component {
     render() {
         return (
-            <View>
-                <Button
-                    title="Flood"
-                    onPress={() => this.props.navigation.navigate('Flood')}
-                    color = 'blue'
-                />
-                <Button
-                    title="Rain"
-                    onPress={() => this.props.navigation.navigate('Rain')}
-                    color = 'red'
-                />
+            <View style = {styles.container}>
+                <TouchableOpacity
+                onPress={() => this.props.navigation.navigate('Flood')}
+                style={styles.button}>
+                    <Text style = {styles.text}>
+                        Flood
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                onPress={() => this.props.navigation.navigate('Rain')}
+                style={styles.button}>
+                    <Text style = {styles.text}>
+                        Rain
+                    </Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#b3e5fc',
+        padding: 8,
+    },
+    text: {
+        fontWeight: 'bold',
+        color: 'black',
+        fontSize: 20,
+        fontFamily: 'monospace'
+    },
+    button:{
+        alignItems: 'center',
+        backgroundColor: '#64b5f6',
+        padding: 10,
+        borderRadius: 6,
+        margin: 2,
+        borderWidth: 0.5,
+    },
+});
