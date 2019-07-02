9ec151eefd57cd7b037e0394193b062d3e4d29d9
Dhiraj Saharia <dsaharia@outlook.com>
2019-07-02 23:33:22 +0530

    android settings for new app icon

--- front_end/android/app/src/main/java/com/indra/MainApplication.java
+++ front_end/android/app/src/main/java/com/indra/MainApplication.java
@@ -3,6 +3,7 @@ package com.indra;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
          // register SQLite Plugin he
           new MainReactPackage(),
+            new MapsPackage(),
             new RNGestureHandlerPackage()
       );
     }
