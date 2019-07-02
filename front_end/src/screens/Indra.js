9bc7ae24a65e01347ebf38afca043357ffdb0b9c
Dhiraj Saharia <dsaharia@outlook.com>
2019-07-02 23:32:32 +0530

    name changed Issue#5

--- front_end/src/screens/Indra.js
+++ front_end/src/screens/Indra.js
@@ -1,55 +1,85 @@
 import React, { Component } from 'react';
-import { YellowBox, View ,Text, StyleSheet, StatusBar} from 'react-native';
+import { YellowBox, View, Alert, Text, StyleSheet, StatusBar } from 'react-native';
 YellowBox.ignoreWarnings([
-  'Warning: isMounted(...) is deprecated',
-  'Module RCTImageLoader',
+    'Warning: isMounted(...) is deprecated',
+    'Module RCTImageLoader',
 ]);
 import Buttons from '../../Buttons';
 import SubmitButton from "../components/SubmitButton";
 import SelectedReport from '../components/SelectedReport';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
+//     componentDidMount(){
+//             Alert.alert("Mounted!")
+// }
+    render() {
+        return (
+            <View style={styles.container}>
           <StatusBar
          backgroundColor="#093F61"
          barStyle="light-content" />
-
+         
          <View style={styles.navbar}>
              <Text style={styles.indra_text}>
-             International Natural DisasteR Archive {'\n'}INDRA
+             Project INDRA {'\n'} International Natural Disaster Research and Analysis
              </Text>
          </View>
          <Buttons navigation={this.props.navigation} />
          <SelectedReport />
+         <View style={styles.mapContainer}>
+                <MapView
+                style={styles.map}
+                  provider={PROVIDER_GOOGLE}
+                  region={{
+                    latitude: 26.052349,
+                    longitude: 92.860390,
+                    latitudeDelta: 0.8,
+                    longitudeDelta: 0.8,
+                  }}>
+                  <MapView.Marker
+                        coordinate={{latitude: 26.052349,
+                        longitude: 92.860390}}
+                        title={"title"}
+                        description={"description"}
+                     />
+                  </MapView>
+          </View>
          <SubmitButton />
       </View>
-    );
+        );
 
-  }
+    }
 }
 
-const styles = StyleSheet.create(
-    {
-            container:
-            {
-                flex:1,
-                backgroundColor: '#D3E4E7'
-            },
-            navbar:
-            {
-                height:55,
-                backgroundColor:'#093F61',
-                elevation:4
-            },
-            indra_text:
-            {
-                color: 'white',
-                fontWeight: 'bold',
-                fontSize: 17,
-                textAlign: 'center',
-                marginTop: 7,
-            },
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#b3e5fc'
+    },
+    mapContainer: {
+        flex: 1,
+        backgroundColor: '#b3e5fc'
+    },
+    navbar: {
+        height: 55,
+        backgroundColor: '#093F61',
+        elevation: 4
+    },
+    indra_text: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 14,
+        textAlign: 'center',
+        marginTop: 8,
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 11,
+        right: 11,
+        bottom: 20,
+  },
 
-    });
+});
