0b0716b5810b29bceb19210b70385e475f8f7f98
Dhiraj Saharia <dsaharia@outlook.com>
2019-07-03 19:49:35 +0530

    deleted files under same folder name

--- frontend/App.js
+++
@@ -1,8 +0,0 @@
-
-import { YellowBox } from 'react-native';
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-import Navigator from "./Navigator";
-import {createAppContainer } from 'react-navigation';
-
-export default createAppContainer(Navigator);
-
--- frontend/index.js
+++
@@ -1,7 +0,0 @@
-
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-// var SQLite = require('react-native-sqlite-storage')
-AppRegistry.registerComponent(appName, () => App);
