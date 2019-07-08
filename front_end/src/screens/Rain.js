77a89c9a0e1a3e9aa836cdde8a6854c8f4553f64
Dhiraj Saharia <dsaharia@outlook.com>
2019-07-08 23:38:57 +0530

    formating changed

--- front_end/src/screens/Rain.js
+++ front_end/src/screens/Rain.js
@@ -8,19 +8,19 @@ import {
     FlatList,
 } from 'react-native';
 
-const colors = ['#8abccf','#73a2c6','#5d8abd','#4771b2','#2e59a8','#00429d',]
+const colors = ['#8abccf', '#73a2c6', '#5d8abd', '#4771b2', '#2e59a8', '#00429d', ]
 
 export default class Rain extends Component {
     render() {
         const rainData = [
-            {key : "Light Rain"},
-            {key : "Heavy Rain"},
-            {key : "Freezing Rain"},
-            {key : "Freezing Drizzle"},
-            {key : "Snow"},
-            {key : "Mixed Rain and Snow"},
+            { key: "Light Rain" },
+            { key: "Heavy Rain" },
+            { key: "Freezing Rain" },
+            { key: "Freezing Drizzle" },
+            { key: "Snow" },
+            { key: "Mixed Rain and Snow" },
         ]
-        
+
         return (
             <View style={styles.container}>
             <FlatList
@@ -42,8 +42,8 @@ export default class Rain extends Component {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#81d4fa',
+        flex: 1,
+        backgroundColor: '#81d4fa',
     },
     button: {
         alignItems: 'center',
