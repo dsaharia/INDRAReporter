764862e3fa7a6c05956278615a33aa39de918d1c
Dhiraj Saharia <dsaharia@outlook.com>
2019-07-11 23:59:50 +0530

    rain updated color

--- front_end/src/screens/Rain.js
+++ front_end/src/screens/Rain.js
@@ -8,11 +8,12 @@ import {
     FlatList,
 } from 'react-native';
 
-const colors = ['#8abccf', '#73a2c6', '#5d8abd', '#4771b2', '#2e59a8', '#00429d', ]
+const colors = ['#91cde3', '#8abccf', '#73a2c6', '#5d8abd', '#4771b2', '#2e59a8', '#00429d', ]
 
 export default class Rain extends Component {
     render() {
         const rainData = [
+            { key: "No Rain" },
             { key: "Light Rain" },
             { key: "Heavy Rain" },
             { key: "Freezing Rain" },
@@ -23,6 +24,9 @@ export default class Rain extends Component {
 
         return (
             <View style={styles.container}>
+            <View style={styles.headerContainer}>
+                <Text style={styles.headerText}> What kind of rain?</Text>
+            </View>
             <FlatList
             data = {rainData}
             renderItem = {
@@ -43,15 +47,30 @@ export default class Rain extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#81d4fa',
+        backgroundColor: '#bdbdbd',
+        paddingLeft: 15,
+        paddingRight: 15,
     },
     button: {
         alignItems: 'center',
+        justifyContent: 'center',
         backgroundColor: '#1e88e5',
+        height: 50,
     },
     rainText: {
         fontSize: 30,
         fontFamily: 'Ariel',
-        color: 'white',
+        color: '#093F61',
     },
+    headerText: {
+        
+        fontSize: 30,
+    },
+    headerContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        paddingBottom: 30,
+        paddingTop: 30,
+    }
 });
