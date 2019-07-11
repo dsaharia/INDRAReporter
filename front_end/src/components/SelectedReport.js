6ecd356d16e4ccf2d6a9c6bc9d1f551e9204d7df
Dhiraj Saharia <dsaharia@outlook.com>
2019-07-12 00:29:12 +0530

    layout changed for text display

--- front_end/src/components/SelectedReport.js
+++ front_end/src/components/SelectedReport.js
@@ -3,31 +3,44 @@ import {Text, View, StyleSheet} from 'react-native';
 
 const SelectedReport = (props) => (
     <View style={styles.container}>
-    <View styles='backgroundColor': 'red'>
-        <Text style={styles.text}>Selected Report: </Text>
-    </View>
-        <Text style={styles.text}> {props.report}</Text>
+        <View style={styles.selectedReport}>
+            <Text style={styles.selectedReportText}>Selected Report: </Text>
+        </View>
+        <View style={styles.reportType}>
+            <Text style={styles.reportText}> {props.report}</Text>
+        </View>
     </View>
 );
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#093F61',
-        paddingTop: 15,
-        paddingLeft: 10,
-        paddingBottom: 15,
-        paddingRight: 15,
-        left: 11,
-        right: 11,
-        bottom: 5,
-        borderRadius: 13,
         flexDirection: 'row',
     },
-    text: {
+    selectedReportText: {
+        fontFamily: 'Ariel',
+        fontSize: 15,
+        color: 'red',
+    },
+    reportText: {
         fontFamily: 'Ariel',
         fontSize: 15,
         color: 'white',
-         // crashed the app?
+    },
+    reportType: {
+        backgroundColor: '#093F61',
+        paddingTop: 13,
+        paddingLeft: 8,
+        paddingBottom: 13,
+        paddingRight: 15,
+        left: 11,
+        bottom: 3,
+        width: 211,
+        borderRadius: 5,
+    },
+    selectedReport: {
+        justifyContent: 'center',
+        paddingLeft: 14,
+
     }
 })
 export default SelectedReport;
