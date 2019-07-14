c4db5946b8e442d1208c2aa818b385958742c115
Dhiraj Saharia <dsaharia@outlook.com>
2019-07-14 23:57:33 +0530

    Flood level added with an image

--- front_end/src/screens/FloodLevel.js
+++ front_end/src/screens/FloodLevel.js
@@ -1,39 +1,45 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, ImageBackground, StyleSheet, View } from 'react-native';
 import VerticalSlider from 'rn-vertical-slider';
-
+import SubmitButton from '../components/SubmitButton';
 
 const FloodLevel = () => (
-        <View style={styles.container}>
-            <VerticalSlider
-              value={1}
-              disabled={false}
-              min={0}
-              max={100}
-              onChange={(value: number) => {
-                console.log("CHANGE", value);
-              }}
-              onComplete={(value: number) => {
-                console.log("COMPLETE", value);
-              }}
-              width={50}
-              height={250}
-              step={1}
-              borderRadius={5}
-              minimumTrackTintColor={"gray"}
-              maximumTrackTintColor={"tomato"}
-              showBallIndicator={true}
-              ballIndicatorColor={"gray"}
-              ballIndicatorTextColor={"white"}
-            />
-        </View>
+  <View style={styles.container}>
+      <ImageBackground source={require('./man.png')} style={{width: '60%', height: '80%'}}>
+            <View style={styles.container}>
+                <VerticalSlider
+                  value={1}
+                  disabled={false}
+                  min={0}
+                  max={100}
+                  // onChange={(value: number) => {
+                  //   console.log("CHANGE", value);
+                  // }}
+                  onComplete={(value) => {
+                    Alert.alert(JSON.stringify(value))
+                  }}
+                  width={70}
+                  height={260}
+                  step={1}
+                  borderRadius={5}
+                  minimumTrackTintColor='#00FFFFFF'
+                  maximumTrackTintColor='transparent'
+                  showBallIndicator={true}
+                  ballIndicatorColor={"red"}
+                  ballIndicatorTextColor={"green"}
+                />
+            </View>
+      </ImageBackground>
+      <SubmitButton />
+    </View>
 );
 
 const styles = StyleSheet.create({
   container: {
-    padding: 70,
+    padding: 14,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'transparent',
   },
 });
 
