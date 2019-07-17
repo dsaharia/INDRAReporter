c64124fe7afe54c4b85b877dd81d3639effce4a0
Dhiraj Saharia <dsaharia@outlook.com>
2019-07-17 23:51:13 +0530

    Flood impact screen added upto Level 2 info using layout

--- front_end/src/screens/FloodImpact.js
+++ front_end/src/screens/FloodImpact.js
@@ -5,28 +5,31 @@ import {
     Text,
     Alert,
     TouchableOpacity,
+    FlatList,
 } from 'react-native';
 
+const reports = [
+    { key: "MINIMAL - Isolated flooding in low-lying areas. Wet road surfaces." },
+    { key: "MINOR - Localized flooding of land and roads. Disruption of travel to key flooding sites." },
+    { key: "SIGNIFICANT - Affecting section of communities. Flooded street and stranded vehicles." },
+    { key: "SEVERE - Affecting entire communities. Buildings swept away. Large scale evacuation." },
+]
+
+const colors = ['#74a9cf', '#0571b0', '#fdae61', '#d7191c']
 export default class FloodImpact extends Component {
-    const reports = [
-        { key: "Minimal" },
-        { key: "Minor" },
-        { key: "Significant" },
-        { key: "Severe" },
-    ]
 
     render() {
         return (
             <View style={styles.container}>
                 <View style = {styles.headerContainer}>
-                    <Text style = {styles.questionText}> What are you seeing?</Text>
+                    <Text style = {styles.questionText}> What are you experiencing?</Text>
                 </View>
 
                 <FlatList 
                 data = {reports}
                 renderItem = {
                     ({item, index}) => <TouchableOpacity
-                    style = {styles.button}
+                    style = {[styles.button, {backgroundColor: colors[index % colors.length]}]}
                     onpress = {() => Alert.alert(item.key)} >
                         <Text style={styles.text}> {item.key}</Text>
                     </TouchableOpacity>
@@ -36,4 +39,34 @@ export default class FloodImpact extends Component {
 
         );
     }
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#b3e5fc',
+        paddingLeft: 15,
+        paddingRight: 15,
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#1e88e5',
+    },
+    text: {
+        fontSize: 22,
+        fontFamily: 'Arial',
+        color: 'white',
+        textAlign: 'left',
+        padding: 5,
+    },
+    questionText: {
+        fontSize: 25,
+    },
+    headerContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: 30,
+        paddingTop: 30,
+    }
+});
