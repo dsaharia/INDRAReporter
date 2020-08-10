f23e51785ec8e8364a654e868fac0bb6afba9a0d
Dhiraj Saharia <dsaharia@outlook.com>
2020-08-10 23:55:49 +0530

    prettier, font-family changed

--- front_end/src/components/SelectedReport.js
+++ front_end/src/components/SelectedReport.js
@@ -1,48 +1,51 @@
-import React, { Component } from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
 
-const SelectedReport = ({report}) => (
-    <View style={styles.container}>
-        <View style={styles.selectedReport}>
-            <Text style={styles.selectedReportText}>Selected Report: </Text>
-        </View>
-        <View style={styles.reportType}>
-            <Text style={styles.reportText}> {report.category} - {report.desc}</Text>
-        </View>
+const SelectedReport = ({ report }) => (
+  <View style={styles.container}>
+    <View style={styles.selectedReport}>
+      <Text style={styles.selectedReportText}>Selected Report: </Text>
     </View>
+    <View style={styles.reportType}>
+      <Text style={styles.reportText}>
+        {" "}
+        {report.category} - {report.desc}
+      </Text>
+    </View>
+  </View>
 );
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-    },
-    selectedReportText: {
-        fontFamily: 'ArialMT',
-        fontSize: 15,
-        color: 'red',
-    },
-    reportText: {
-        fontFamily: 'Arial',
-        fontSize: 15,
-        color: 'white',
-        paddingLeft: 2,
-        textAlign: 'left'
-    },
-    reportType: {
-        backgroundColor: '#093F61',
-        paddingTop: 13,
-        paddingLeft: 8,
-        paddingBottom: 13,
-        paddingRight: 15,
-        left: 11,
-        bottom: 3,
-        width: '59%',
-        borderRadius: 5,
-    },
-    selectedReport: {
-        justifyContent: 'center',
-        paddingLeft: 14,
-
-    }
-})
+  container: {
+    flexDirection: "row",
+  },
+  selectedReportText: {
+    // fontFamily: "cursive",
+    fontFamily: "fantasy",
+    fontSize: 17,
+    color: "#12355B",
+  },
+  reportText: {
+    fontFamily: "fantasy",
+    fontSize: 15,
+    color: "white",
+    paddingLeft: 2,
+    textAlign: "left",
+  },
+  reportType: {
+    backgroundColor: "#093F61",
+    paddingTop: 13,
+    paddingLeft: 8,
+    paddingBottom: 13,
+    paddingRight: 15,
+    left: 11,
+    bottom: 3,
+    width: "59%",
+    borderRadius: 5,
+  },
+  selectedReport: {
+    justifyContent: "center",
+    paddingLeft: 14,
+  },
+});
 export default SelectedReport;
